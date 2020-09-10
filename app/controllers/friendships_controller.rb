1c770b2d565939d6cf90c6841dcecf3d5143c940
Elijah <eligenius@gmail.com>
2020-09-10 17:35:25 +0100

    Add create and new method to friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,17 @@
 class FriendshipsController < ApplicationController
+  def new
+    @friendship = Friendship.new
+  end
+
+  def create
+    @friendship_request = Friendship.new(params[:friendship_request])
+    if @friendship_request.save
+      flash[:success] = "Friendship successfully created"
+      redirect_to users_path
+    else
+      flash[:error] = "Something went wrong"
+    end
+  end
+  
+  
 end
