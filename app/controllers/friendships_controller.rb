7704f140f5177c45b9b3a2d2294bd1e8f2fd3a97
Elijah <eligenius@gmail.com>
2020-09-10 18:43:47 +0100

    Add update method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,14 +4,25 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @friendship_request = Friendship.new(params[:friendship_request])
+    @friendship_request = current_user.Friendship.new(id: params[:id], user_id: params[user_id:], friend_id:, status: false)
     if @friendship_request.save
-      flash[:success] = "Friendship successfully created"
+      flash[:success] = "Friend request sent"
       redirect_to users_path
+    else
+      flash[:error] = "Unable to send friend request"
+    end
+  end
+
+  def update
+    @friendship = current_user.Friendship.find(id: params[:id])
+    if @friendship.update(status: true)
+      flash[:success] = "Friend request accepted"
+      redirect_to @users_path
     else
       flash[:error] = "Something went wrong"
     end
   end
+  
   
   
 end
