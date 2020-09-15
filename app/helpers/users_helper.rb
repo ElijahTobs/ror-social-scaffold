a74a39b84ad87a2aa4675695b06173f978d2444e
Elijah <eligenius@gmail.com>
2020-09-15 11:36:37 +0100

    Add logic for friendship status in users_helper, update routes and users_view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,20 @@
 module UsersHelper
   def friendship_status(obj)
-    
+    html = ""
+
+    if current_user.friend_requests.include?(obj)
+      html += link_to "Accept", friendship_path(create_params), method: :put
+      html += "  "
+      html += link_to "Reject", action: "destroy", id: delete
+    elsif current_user.pending_friends.include?(obj)
+      html += "Pending Request"
+    elsif current_user.friends.include?(obj)
+      html += "Already Friends"
+    else
+      html += link_to "Invite", action: "new"
+    end
+
+    html.html.safe
   end
-  
-  
+
 end
