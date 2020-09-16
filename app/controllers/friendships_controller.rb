4e74558fe6a49d2a95db142d4a1c7423112fda04
Elijah <eligenius@gmail.com>
2020-09-16 22:55:11 +0100

    Delete useless comments

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,20 +1,6 @@
 class FriendshipsController < ApplicationController
-  # def new
-  #   @friendship = Friendship.new
-  # end
-  
-  # accept_params = { id: friend[0], user_id: current_user.id, friend_id: id, status: true }
-
-  # def index
-  #   @friendship = Friendship.all
-  #   @friendship
-  # end
-
   def create
-    # @request_sender = User.find(params[:user_id])
     @friendship = Friendship.new(friendship_params)
-
-    # user_id: current_user.id, friend_id: @request_sender.id, confirmed: false
 
     if @friendship.save
       flash[:notice] = "Friend request sent"
@@ -25,8 +11,6 @@ class FriendshipsController < ApplicationController
   end
 
   def update
-    # @request_sender = User.find(params[:user_id])
-    # @friendship = Friendship.find(params[:id])
     @user = User.find(friend_id: params[:user_id])
 
     if current_user.confirm_friend(@user, method: :put)
