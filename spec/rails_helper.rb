00a766a363c9370e86c774fc4790c2b03c3f228c
Elijah <eligenius@gmail.com>
2020-10-06 15:58:33 +0100

    Edit rails_helper.rb to fix rspec error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,8 +51,24 @@ RSpec.configure do |config|
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
+  
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 
   # start the transaction strategy as examples are run
   config.around(:each) do |example|
