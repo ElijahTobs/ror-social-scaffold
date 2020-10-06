7594767d2710ccb628b371216b50cf99cec185e4
Elijah <eligenius@gmail.com>
2020-10-06 15:33:06 +0100

    Fix rspec error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@
 # files.
 #
 
-require 'database_cleaner'
+# require 'database_cleaner'
 # Given that it is always loaded, you are encouraged to keep this file as
 # light-weight as possible. Requiring heavyweight dependencies from this file
 # will add to the boot time of your test suite on EVERY test run, even for an
