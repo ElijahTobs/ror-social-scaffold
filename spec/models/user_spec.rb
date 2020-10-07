27e2aec966782b322aef2ea23aa49c7701e6da9a
Elijah <eligenius@gmail.com>
2020-10-07 15:45:10 +0100

    Refactor tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,39 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_length_of(:name).is_at_most(20) }
-    it { should validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(20) }
+    it { is_expected.to validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
 
-    let(:wrong) { 'test@test' }
-    it { should_not allow_value(:wrong).for(:email) }
+    # let(:wrong) { 'test@test' }
+    # it { should_not allow_value(:wrong).for(:email) }
 
-    it { should allow_value('test@test.com').for(:email) }
+    # it { should allow_value('test@test.com').for(:email) }
+
+    context 'when email has wrong format' do
+      let(:wrong_email1) { 'jan@foo' }
+  
+      it 'complains for invalid format' do
+        # is_expected.to eq false
+        should_not allow_value(:wrong_email1).for(:email)
+      end
+  
+      let(:wrong_email2) { 'jan' }
+  
+      it 'complains for invalid format' do
+        # is_expected.to eq false
+        should_not allow_value(':wrong_email2').for(:email)
+      end
+    end
+  
+    context 'when email has correct format' do
+      it 'accepts valid format' do
+        is_expected.to allow_value('good@mail.com').for(:email)
+        # should allow_value(:correct_email).for(:email)
+      end
+    end
+
   end
   describe 'associations' do
     it { should have_many(:posts) }
